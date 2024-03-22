aab41a9664220e010075811d15b80f834910878a
Karrrramba <137409525+Karrrramba@users.noreply.github.com>
2024-03-22 14:20:40 +0100

    Added map

--- code/assignment_code.R
+++ code/assignment_code.R
@@ -1,16 +1,17 @@
 library(dplyr)
 library(ggplot2)
 library(grid)
+library(leaflet)
 library(magrittr)
 library(readr)
 library(stringr)
 library(tidyr)
 
 library(tidyverse)
+library(ggmap)
 
 
-
-# Data prep -------
+# Data prep -----
 
 ext_tracks_widths <- c(7, 10, 2, 2, 3, 5, 5, 6, 4, 5, 4, 4, 5, 3, 4, 3, 3, 3,
                        4, 3, 3, 3, 4, 3, 3, 3, 2, 6, 1)
@@ -40,4 +41,24 @@ tracks_clean <- ext_tracks %>%
                ) %>% 
   pivot_wider(names_from = Quadrant,
               values_from = Radius)
+
+# Mapping -----
+Ike <- tracks_clean %>% 
+  filter(str_starts(Storm_ID, "IKE"))
+
+Ike_map <- get_map(c(left = min(Ike$Longitude), bottom = min(Ike$Latitude), 
+          right = max(Ike$Longitude), top = max(Ike$Latitude)), 
+        source = "stadia", maptype = "stamen_toner_background", zoom = 6) %>%
+  ggmap(extent = "device")
+
+# +
+#   geom_hurricane(data = storm_observation,
+#                  aes(x = longitude, y = latitude, 
+#                      r_ne = ne, r_se = se, r_nw = nw, r_sw = sw,
+#                      fill = wind_speed, color = wind_speed)) + 
+#   scale_color_manual(name = "Wind speed (kts)", 
+#                      values = c("red", "orange", "yellow")) + 
+#   scale_fill_manual(name = "Wind speed (kts)", 
+#                     values = c("red", "orange", "yellow"))
+
 
