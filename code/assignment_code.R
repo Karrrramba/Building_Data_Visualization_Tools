98eea317193fd8192a95a4124e3fb6e7f86436a1
Karrrramba <137409525+Karrrramba@users.noreply.github.com>
2024-03-22 12:20:48 +0100

    Data prep

--- code/assignment_code.R
+++ code/assignment_code.R
@@ -8,10 +8,36 @@ library(tidyr)
 
 library(tidyverse)
 
-colnames(hurricanes) <- c("ID", "Name", "Date", "Year", "Latitude", "Longitude",
-                          "Knot_34", "Knot_50", "Knot_64")
 
-hr_txt <- readLines("data/ebtrk_atlc_1988_2015.txt", n = -1,)
-hr_txt <- gsub("NOT\\s+NAMED", "NOTNAMED", hr_txt)
-hurricanes <- read.table(text = gsub("\\s{2,}", " ", hr_txt), 
-                         sep = " ", header = F, fill = TRUE, na.strings = "-99")
+
+# Data prep -------
+
+ext_tracks_widths <- c(7, 10, 2, 2, 3, 5, 5, 6, 4, 5, 4, 4, 5, 3, 4, 3, 3, 3,
+                       4, 3, 3, 3, 4, 3, 3, 3, 2, 6, 1)
+ext_tracks_colnames <- c("Storm_ID", "Storm_Name", "Month", "Day",
+                         "Hour", "Year", "Latitude", "Longitude",
+                         "Max_Wind", "Min_Pressure", "Rad_Max_Wind",
+                         "Eye_Diameter", "Pressure_1", "Pressure_2",
+                         paste("Radius_34", c("NE", "SE", "SW", "NW"), sep = "_"),
+                         paste("Radius_50", c("NE", "SE", "SW", "NW"), sep = "_"),
+                         paste("Radius_64", c("NE", "SE", "SW", "NW"), sep = "_"),
+                         "Storm_Type", "Distance_to_Land", "Final")
+
+ext_tracks <- read_fwf("data/ebtrk_atlc_1988_2015.txt", 
+                       fwf_widths(ext_tracks_widths, ext_tracks_colnames),
+                       na = "-99")
+
+tracks_clean <- ext_tracks %>% 
+  mutate(Longitude = round(Longitude - 180, 1),
+         Date = ymd_h(paste0(Year, Month, Day, Hour)),
+         Storm_ID = paste0(Storm_Name, "-", Year)) %>% 
+  select(!c(2:6, 9:14, 27:29)) %>% 
+  relocate(Date, .after = Storm_ID) %>% 
+  pivot_longer(cols = starts_with("Radius"),
+               names_to = c("Wind_Speed", "Quadrant"),
+               names_pattern = "Radius_(.*)_(.*)",
+               values_to = "Radius",
+               ) %>% 
+  pivot_wider(names_from = Quadrant,
+              values_from = Radius)
+
