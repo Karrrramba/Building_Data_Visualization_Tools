bb4394f438cdaeb412468a961c2c05c674f42000
Karrrramba <137409525+Karrrramba@users.noreply.github.com>
2024-03-28 14:16:18 +0100

    Geom_hurricane working

--- code/geom_hurricane.R
+++ code/geom_hurricane.R
@@ -1,28 +1,50 @@
 library(dplyr)
+library(geosphere)
 library(grid)
 library(ggplot2)
 library(magrittr)
 
-StatRadius <- ggplot::ggproto("StatRadius", Stat,
-                      required_aes = c("long", "lat", "wind_speed", 
-                                       "rad_ne", "rad_nw", "rad_se", "rad_sw"),
-                      
-                      compute_group = function(data, scales, wind_speed_levels) {
-                        projected_points <- lapply(wind_speed_levels, function(speed) {
-                          subset_data <- subset(data, wind_speed == speed)
-                          
-                        })
-                           
-                      }
+StatRadius <- ggplot2::ggproto("StatRadius", Stat,
+                               required_aes = c("x", "y", "rad_ne", "rad_se", "rad_sw", "rad_nw"),
+                               
+                               # default_aes = ggplot2::aes(scale_radii = 1),
+                               
+                               compute_group = function(data, scales, rad_ne, rad_se, rad_sw, rad_nw, scale_radii) {
+                                 
+                                 # data <- data %>% 
+                                 #   dplyr::mutate(
+                                 #     dplyr::across(
+                                 #       c(rad_ne, rad_se, rad_sw, rad_nw), ~ .x * 1852
+                                 #     ))
+                                 
+                                 coords <- c(data$x[1], data$y[1])
+                                 
+                                 deg_NE <- 1:90
+                                 deg_SE <- 91:180
+                                 deg_SW <- 181:270
+                                 deg_NW <- 271:360
+                                 
+                                 q_1 <- geosphere::destPoint(coords, b = deg_NE, d = data$rad_ne)
+                                 q_2 <- geosphere::destPoint(coords, b = deg_SE, d = data$rad_se)
+                                 q_3 <- geosphere::destPoint(coords, b = deg_SW, d = data$rad_sw)
+                                 q_4 <- geosphere::destPoint(coords, b = deg_NW, d = data$rad_nw)
+                                 
+                                 point_matrix <- rbind(q_1, q_2, q_3, q_4)
+                                 # point_matrix[ , "lat"] <- point_matrix[ , "lat"] * scale_radii
+                                 point_matrix <- rbind(point_matrix, point_matrix[1, ])
+                                 
+                                 point_df <- data.frame(x = point_matrix[, 1], y = point_matrix[, 2])
+                                 return(point_df)
+                               }
+                               
 )
 
 stat_radius <- function(mapping = NULL, 
                         data = NULL, 
                         geom = "polygon",
-                        scale_factor = 1,
                         position = "identity", 
+                        scale_radii = 1,
                         show.legend = NA,
-                        outliers = TRUE, 
                         inherit.aes = TRUE, 
                         ...) {
   ggplot2::layer(
@@ -30,30 +52,26 @@ stat_radius <- function(mapping = NULL,
     data = data, 
     mapping = mapping, 
     geom = geom, 
-    scale_factor = scale_factor,
     position = position, 
     show.legend = show.legend, 
     inherit.aes = inherit.aes,
-    params = list(outliers = outliers, ...)
+    params = list(scale_radii = scale_radii, ...)
   )        
 }
 
 
-GeomHurricane <- ggplot::ggproto("GeomHurricane", Geom,
-                         required_aes = c("long", "lat", "rad_ne",
-                                          "rad_se", "rad_nw", "rad_sw"), 
-                         default_aes = aes(scale_radii = 1), 
-                         draw_key = draw_key_polygon,
-                         draw_panel = function(data, panel_scales, coord) {
-                         
-                           )
-                         }
+GeomHurricane <- ggplot2::ggproto("GeomHurricane", GeomPolygon,
+                         default_aes = aes(color = "yellow", 
+                                           fill = "yellow", 
+                                           scale_radii = 1, 
+                                           linewidth = 0.5,
+                                           alpha = 0.5)
 )
 
 
 geom_hurricane <- function(mapping = NULL, 
                            data = NULL, 
-                           stat = "hurricane",
+                           stat = "radius",
                            scale_radii = 1,
                            position = "identity", 
                            na.rm = FALSE,
@@ -61,13 +79,27 @@ geom_hurricane <- function(mapping = NULL,
                            inherit.aes = TRUE, 
                            ...) {
   ggplot2::layer(
+    stat = StatRadius,
     geom = GeomHurricane, 
     mapping = mapping,  
     data = data, 
-    stat = stat, 
     position = position, 
     show.legend = show.legend, 
     inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
   )
 }
+
+
+get_map(c(left = min(Ike$Longitude) + 20, bottom = min(Ike$Latitude), 
+          right = max(Ike$Longitude) + 20, top = max(Ike$Latitude)), 
+        source = "stadia", maptype = "stamen_toner_background", zoom = 5) %>%
+  ggmap(extent = "device") +
+  geom_hurricane(data = Ike_34, 
+               aes(x = Longitude, y = Latitude, 
+                   rad_ne = NE, rad_se = SE, rad_sw = SW, rad_nw = NW, 
+                   fill = Wind_Speed, color = Wind_Speed)) +
+  scale_color_manual(name = "Wind speed (kts)",
+                     values = c("red", "orange", "yellow")) +
+  scale_fill_manual(name = "Wind speed (kts)",
+                    values = c("red", "orange", "yellow"))
